7b1b58ae89a7f969b5bc5559196eeea5602d74a1
Yuvaraj93r5 <yuvaraj.g2024@vitstudent.ac.in>
2026-09-10 14:36:49 +0530

    Add Location record with distance calculation method

---
+++ src/main/java/com/emergency/model/Location.java
@@ -0,0 +1,7 @@
+package com.emergency.model;
+
+public record Location(double x, double y) {
+    public double distanceTo(Location other) {
+        return Math.sqrt(Math.pow(this.x - other.x, 2) + Math.pow(this.y - other.y, 2));
+    }
+}
