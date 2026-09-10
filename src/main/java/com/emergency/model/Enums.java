7a8fce71e5cf4f01e191623153eab46e28823c36
Yuvaraj93r5 <yuvaraj.g2024@vitstudent.ac.in>
2026-09-10 14:35:46 +0530

    Add Enums for priority, ambulance type, and status

---
+++ src/main/java/com/emergency/model/Enums.java
@@ -0,0 +1,7 @@
+package com.emergency.model;
+
+public class Enums {
+    public enum Priority { CRITICAL, HIGH, MODERATE, NORMAL }
+    public enum AmbulanceType { BASIC, ADVANCED_LIFE_SUPPORT, ICU }
+    public enum AmbulanceStatus { AVAILABLE, DISPATCHED, EN_ROUTE, PATIENT_PICKED_UP, HOSPITAL_ARRIVED }
+}
